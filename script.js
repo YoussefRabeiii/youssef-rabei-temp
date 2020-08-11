5cc032f3674cebd642915b812d75bd8e0bee4ce6
Youssef Rabei <youssefrabei2003@gmail.com>
2020-08-11 11:57:17 +0200

    Revert "✨ Make it PWA"
    This reverts commit 0e0798ac3d7e9cb8f51ac8bf87d1e6c5bc1169d8.

--- script.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((res) => console.log("🚀 service worker registered"))
-      .catch((err) => console.log("🤬 service worker not registered", err));
-  });
-}
